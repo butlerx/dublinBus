b5a02d5cf516e1452bfda0ab72b837130fc325ee
butlerx <butlerx@notthe.cloud>
2017-06-27 13:10:20 +0100

    v1.2.0

--- example.js
+++ example.js
@@ -1,20 +1,26 @@
 'use strict';
 const dBus = require('.');
 
-dBus.stopAddress(7602).then(address => {
-  console.log(address);
+dBus.stopAddress(1344).then(address => {
+  console.log(`address: ${address}`);
 }).catch(reason => {
   console.log(reason);
 });
 
-dBus.getStopInfo(7602).then(buses => {
-  console.log(buses);
+dBus.getStopInfo(1344).then(info => {
+  console.log(`stop: ${info.stop}'s buses: ${JSON.stringify(info.buses)}`);
 }).catch(reason => {
   console.log(reason);
 });
 
-dBus.getBusesInfo(7602, [44]).then(buses => {
-  console.log(buses);
+dBus.getBusesInfo(1344, [16]).then(buses => {
+  console.log(`just the 16 bus: ${JSON.stringify(buses)}`);
+}).catch(reason => {
+  console.log(reason);
+});
+
+dBus.realTime(1344).then(buses => {
+  console.log(`real time: ${JSON.stringify(buses)}`);
 }).catch(reason => {
   console.log(reason);
 });
