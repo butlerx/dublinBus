34215e26137abd54549ed4616cf4c0541c39bd07
butlerx <butlerx@notthe.cloud>
2017-07-12 19:30:46 +0100

    switch linter

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-'use strict';
-
 const realTime = require('./lib/realTime');
-const stop = require('./lib/stop');
+const stopLib = require('./lib/stop');
+
+const getStopInfo = (stopNum, length) => new Promise((resolve, reject) => {
+  realTime.getInfo(stopNum, length).then(buses => {
+    stopLib.getInfo(stopNum).then(({ address }) => resolve({
+      stop: address,
+      buses,
+    }));
+  }).catch(reject);
+});
 
 const getStopInfoForBuses = (stopNum, busNums) => new Promise((resolve, reject) => {
-  getStopInfo(stopNum).then(({stop, buses}) => {
+  getStopInfo(stopNum).then(({ stop, buses }) => {
     const filteredBuses = [];
     buses.forEach(bus => {
       if (busNums.includes(bus.route)) filteredBuses.push(bus);
     });
-    if (filteredBuses.length > 0) resolve({
-      stop : stop,
-      buses: filteredBuses,
-    });
+    if (filteredBuses.length > 0) resolve({ stop, buses: filteredBuses });
     reject(new Error('No buses for specified routes at this stop.'));
-  }).catch(reject);
-});
-
-const getStopInfo = (stopNum, length) => new Promise((resolve, reject) => {
-  realTime.getInfo(stopNum, length).then(buses => {
-    stop.getInfo(stopNum).then(({address}) => resolve({
-      stop : address,
-      buses: buses,
-    }));
   }).catch(reject);
 });
 
 const getBusesInfo = (stopNum, busNums) => new Promise((resolve, reject) => {
   getStopInfoForBuses(stopNum, busNums)
-    .then(({buses}) => resolve(buses))
+    .then(({ buses }) => resolve(buses))
     .catch(reject);
 });
 
@@ -37,8 +32,8 @@ module.exports = {
   getBusesInfo,
   realTime   : realTime.getInfo,
   realTimeRaw: realTime.getInfoRaw,
-  stopInfo   : stop.getInfo,
-  stopInfoRaw: stop.getInfoRaw,
-  stopAddress: stop.getAddress,
-  stopBuses  : stop.getBuses,
+  stopInfo   : stopLib.getInfo,
+  stopInfoRaw: stopLib.getInfoRaw,
+  stopAddress: stopLib.getAddress,
+  stopBuses  : stopLib.getBuses,
 };
--- example.js
+++ example.js
@@ -1,26 +1,18 @@
-'use strict';
 const dBus = require('.');
 
-dBus.stopAddress(1344).then(address => {
-  console.log(`address: ${address}`);
-}).catch(reason => {
-  console.log(reason);
-});
+dBus.stopAddress(1344).then(address => console.log(`address: ${address}`)).catch(console.log);
 
-dBus.getStopInfo(1344).then(info => {
-  console.log(`stop: ${info.stop}'s buses: ${JSON.stringify(info.buses)}`);
-}).catch(reason => {
-  console.log(reason);
-});
+dBus
+  .getStopInfo(1344)
+  .then(info => console.log(`stop: ${info.stop}'s buses: ${JSON.stringify(info.buses)}`))
+  .catch(console.log);
 
-dBus.getBusesInfo(1344, [16]).then(buses => {
-  console.log(`just the 16 bus: ${JSON.stringify(buses)}`);
-}).catch(reason => {
-  console.log(reason);
-});
+dBus
+  .getBusesInfo(1344, [16])
+  .then(buses => console.log(`just the 16 bus: ${JSON.stringify(buses)}`))
+  .catch(console.log);
 
-dBus.realTime(1344).then(buses => {
-  console.log(`real time: ${JSON.stringify(buses)}`);
-}).catch(reason => {
-  console.log(reason);
-});
+dBus
+  .realTime(1344)
+  .then(buses => console.log(`real time: ${JSON.stringify(buses)}`))
+  .catch(console.log);
